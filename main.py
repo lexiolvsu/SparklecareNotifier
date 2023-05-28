8137a1234674872a90e771ffec37757a93fe7769
lexio <llvsu@outlook.com>
2023-05-28 16:19:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 import requests
+from bs4 import BeautifulSoup
+import hashlib
 import time
 from win10toast import ToastNotifier
 
-def check_comic4():
-    url = "http://sparklecarehospital.com/comic4"
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-    
-    # Send a GET request to the comic4 page
-    response = requests.get(url, headers=headers)
-    
-    # Check if the page exists
-    if response.status_code == 200:
-        # Check if the page content indicates a new comic page
-        if "New Comic Page" in response.text:
-            # Create a notification
-            toaster = ToastNotifier()
-            toaster.show_toast("New Comic Page",
-                               "A new comic page is available!",
-                               duration=10)
-            
-    # Wait for 1 minute before checking again
-    time.sleep(60)
-    check_comic4()
+# URL of the webpage to monitor
+url = 'http://sparklecarehospital.com'
 
-# Start checking for new comic pages
-check_comic4()
+# Initialize the notification object
+toaster = ToastNotifier()
+
+# Variable to store the hash of the previous content
+previous_hash = None
+
+while True:
+    # Send a request to the webpage
+    response = requests.get(url)
+    
+    # Parse the HTML content
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Find the div element with class "latest"
+    latest_div = soup.find('div', {'class': 'latest'})
+    
+    # Calculate the hash of the current content
+    current_hash = hashlib.sha256(latest_div.encode()).hexdigest()
+    
+    # Check if the content has changed
+    if current_hash != previous_hash:
+        # Create a notification
+        toaster.show_toast("New Sparklecare Update!",
+                           "The main page now indicates a new update!",
+                           duration=10)
+        
+        # Update the previous hash
+        previous_hash = current_hash
+    
+    # Wait for some time before checking again (e.g., 1 hour)
+    time.sleep(1000)
